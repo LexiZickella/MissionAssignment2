f78ef0c655c581c46f9fd25d2aa4799188d4ceb2
lexizickella <lexizickella@LEXIZICKELLB450>
2023-01-18 22:12:40 -0700

    Add project files.

---
+++ MissionAssignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MissionAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator! How many dice rolls would you like to simulate ? ");
+        }
+    }
+}
