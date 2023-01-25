782722d53885ad212eb06402bbbc66b68a905ccb
lexizickella <lexizickella@LEXIZICKELLB450>
2023-01-24 19:13:02 -0700

    Final changes for Mission 2

--- MissionAssignment2/Program.cs
+++ MissionAssignment2/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 
-namespace MissionAssignment2
+namespace DiceRoller
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the dice throwing simulator! How many dice rolls would you like to simulate ? ");
+            // Create an array to keep track of the number of times each total is thrown
+            int[] totals = new int[13];
+            // Welcome player and ask the user how many times to roll the dice
+            Console.Write("Welcome to the dice throwing simulator!");
+            Console.WriteLine();
+            Console.Write("Enter the number of times to roll the dice: ");
+            int rolls = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            // Print results of dice rolls
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each '*' represents 1 % of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + rolls);
+            Console.WriteLine();
+
+            // Roll the dice the specified number of times
+            Random rnd = new Random();
+            for (int i = 0; i < rolls; i++)
+            {
+                int die1 = rnd.Next(1, 7);
+                int die2 = rnd.Next(1, 7);
+                int total = die1 + die2;
+                totals[total]++;
+            }
+
+            // Print the histogram
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write(i + ": ");
+                double percentage = (double)totals[i] / rolls * 100;
+                int stars = (int)percentage;
+                for (int j = 0; j < stars; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            // Print exit message to user 
+            Console.WriteLine();
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+            Console.ReadKey();
         }
     }
 }
